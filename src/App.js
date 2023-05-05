6e83dc8d90e7a7d711de361676babb3c298027f8
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-05-05 11:33:22 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 import Header from "./components/header/Header.jsx";
+import Main from "./components/main/Main.jsx";
 
 function App() {
   return (
-    <Header />
+    <div className="mainContainer">
+      <div className="header">
+        <Header />
+        </div>
+      <div className="container">
+        <Main />
+      </div>
+   
+    </div>
   );
 }
 
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,17 @@
+import "./main.scss"
+
+function Main () {
+    return(
+        <div className="main">
+            <div className="mainButtonAdd">
+            <button className="button"> <p>+</p> </button>
+            <button className="button--Add">Add New Task</button></div>
+
+
+        </div>
+    );
+
+
+}
+
+export default Main;
