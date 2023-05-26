1c3dcb37b434379d9304667e589215783f4a3575
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-05-26 14:41:50 +0300

    last commit

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,6 +2,7 @@ import "./main.scss"
 import React, { useState } from "react";
 import bagSvg from '/Projects/reactProjects/my-todo-list/src/assets/img/bag.png'
 import penSvg from '/Projects/reactProjects/my-todo-list/src/assets/img/pen.png'
+import dickSvg from '/Projects/reactProjects/my-todo-list/src/assets/img/dick.png'
 
 import Input from "../input/Input";
 import Button from "../button/Button";
@@ -10,26 +11,26 @@ import {v4 as uuidv4 } from "uuid";
 
 function Main () {
     const [taskMainAll, setTaskMainAll] = useState([]); //хранилище тасок.
-    const [value, setValue] = useState(''); //хранилище input, setValue- изменение значения value.
-    const [selectedItem, setSelectedItem] = useState('');
+    const [value, setValue] = useState(''); //state(хранилище) input, setValue- изменение значения value.
+    const [selectedItem, setSelectedItem] = useState(''); // хранилище
+    const [editTitle, setEditTitle] = useState('');// хранилище тайтла выбраной таски 
 
     const getValueInput = (event) => {
         setValue(event.target.value); //текст с инпута ложим в свой 'State' setValue (target.value(свойство ивента) target -ссылка на объект)
        //  console.log(event);
     }
-    
+
     const addTask = () => {
         const task = {
             text: value,
             checked: false,
-            id: uuidv4(),
+            id: uuidv4(), //динамический id
         }
         if (value !== '') { 
             setTaskMainAll([...taskMainAll, task])   
-            setValue(''); 
+            setValue(''); // сбрасывает стате инпута на пустую строку
     }
          // ... spread оператор  развернули массив, таск добавили
-     
     }
     const checkBoxOn = (id) => {
         const changeTaskMainAll = taskMainAll.map((task) => {
@@ -42,19 +43,30 @@ function Main () {
             return task
         })   
         setTaskMainAll(changeTaskMainAll)
-    }
-    
+    }  
+
     const delTask = (id) => {
         setTaskMainAll(taskMainAll.filter(obj => obj.id !== id))
         };
 
-    const changeTask = (id, text) =>{
+    const editTask = (id, text) => {
         
-        setSelectedItem(id);
+        setSelectedItem(id);// ложиться в стейт
+        setEditTitle(text);// ложиться в стейт
 
-        console.log(id)
-        console.log(text)
     }
+
+    const changeTitle = (event) => {
+
+        setEditTitle(event.target.value)
+    }
+
+    const saveNewTask = () => {
+        
+      
+    }
+    
+    
 
     return(
         <div className="main">
@@ -67,9 +79,10 @@ function Main () {
             <div key={index}> 
             <div className={task.checked ? 'linethrou goal' : 'goal'} >   
                 <button className="mainButtonAdd" onClick={() => delTask(task.id) }><img width="25" src={bagSvg} alt="bag"  /></button>
-                <button className="mainButtonAdd" onClick={() => changeTask(task.id, task.text)}><img width="30" src={penSvg} alt="pen" /></button>
+                <button className="mainButtonAdd" onClick={() => editTask(task.id, task.text)} ><img width="30" src={penSvg} alt="pen" /></button>
                 <Input  type='checkbox' checked={task.checked} onChange={() => checkBoxOn(task.id)}/>
-                {selectedItem === task.id ? <Input value={task.text}/> : task.text}</div>
+                {selectedItem === task.id ? <Input value={editTitle} onChange={(event) => changeTitle(event)}/> : task.text}
+                 <button className="mainButtonAdd"  onClick={() => saveNewTask()} ><img width="28" src={dickSvg} alt="pen" /></button></div>
             </div>)}
         </div>
     );  
